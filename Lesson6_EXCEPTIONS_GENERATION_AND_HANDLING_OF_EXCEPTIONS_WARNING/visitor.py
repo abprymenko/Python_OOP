a9f77f0e3bf0d074698296be1086e0d6ef414ca0
Andrii Prymenko <127588413+abprymenko@users.noreply.github.com>
2024-11-16 21:21:21 +0100

    Update visitor.py

--- Lesson6_EXCEPTIONS_GENERATION_AND_HANDLING_OF_EXCEPTIONS_WARNING/visitor.py
+++ Lesson6_EXCEPTIONS_GENERATION_AND_HANDLING_OF_EXCEPTIONS_WARNING/visitor.py
@@ -1,17 +1,13 @@
-class DigitVisitor:
+class Parser:
     @staticmethod
     def IntTryParse(value:str):
-        try:
-            if isinstance(value, int):
-                return int(value)
-            return None
-        except ValueError as ve:
-            raise ve
+        if isinstance(value, int):
+            return int(value)
+        return None
+
     @staticmethod
     def FloatTryParse(value:str):
-        try:
-            if isinstance(value, float):
-                return float(value)
-            return None
-        except ValueError as ve:
-            raise ve
+        if isinstance(value, float):
+            return float(value)
+        return None
+        
